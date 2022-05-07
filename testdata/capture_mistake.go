9f61984a7e93ba28cca6baa9ed35ae09addd42bd
Devin Gunay <devingunay@gmail.com>
2022-05-06 23:24:03 -0700

    failing test of call on receiver through array indexing

--- testdata/capture_mistake.go
+++ testdata/capture_mistake.go
@@ -26,10 +26,12 @@ func main() {
 	outer := MyImpl{}
 	outer2 := HasMyImpl{A: MyImpl{}}
 	outer3 := struct{ B HasMyImpl }{B: HasMyImpl{A: MyImpl{}}}
+	outerArr := []MyImpl{{}}
 	doThing(func(inner MyInterface) {
-		outer.Do()      // want "captured variable outer implements interface MyInterface"
-		outer2.A.Do()   // want "captured variable outer2.A implements interface MyInterface"
-		outer3.B.A.Do() // want "captured variable outer3.B.A implements interface MyInterface"
+		outer.Do()       // want "captured variable outer implements interface MyInterface"
+		outer2.A.Do()    // want "captured variable outer2.A implements interface MyInterface"
+		outer3.B.A.Do()  // want "captured variable outer3.B.A implements interface MyInterface"
+		outerArr[0].Do() // want "captured variable outerArr[0] implements interface MyInterface"
 		inner.Do()
 	})
 }
